a4e7b28e54e301e4d3630421d380c8a15df5b5ae
Andrei Draganescu <andrei.draganescu@automattic.com>
2023-11-28 14:26:57 +0200

    rebase cleanup

--- packages/edit-site/src/components/editor/index.js
+++ packages/edit-site/src/components/editor/index.js
@@ -185,7 +185,6 @@ export default function Editor( { listViewToggleElement, isLoading } ) {
 	);
 
 	const settings = useSpecificEditorSettings();
-
 	const isReady =
 		! isLoading &&
 		( ( postWithTemplate && !! contextPost && !! editedPost ) ||
