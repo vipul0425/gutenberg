c6d064dc756def7384fd2858946939b14d89877d
Andrew Serong <14988353+andrewserong@users.noreply.github.com>
2023-11-29 15:32:01 +1100

    Try displacing list view items via transform: translateY while dragging

--- packages/block-editor/src/components/list-view/block.js
+++ packages/block-editor/src/components/list-view/block.js
@@ -43,6 +43,7 @@ import AriaReferencedText from './aria-referenced-text';
 
 function ListViewBlock( {
 	block: { clientId },
+	displacement,
 	isDragged,
 	isSelected,
 	isBranchSelected,
@@ -269,6 +270,8 @@ function ListViewBlock( {
 		'is-dragging': isDragged,
 		'has-single-cell': ! showBlockActions,
 		'is-synced': blockInformation?.isSynced,
+		'is-above': displacement === 'above',
+		'is-below': displacement === 'below',
 	} );
 
 	// Only include all selected blocks if the currently clicked on block
