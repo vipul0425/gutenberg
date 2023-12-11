2544cbe12eb3a7ad5e86cce09c0460da114aa6cd
Dave Smith <getdavemail@gmail.com>
2023-12-11 17:52:42 +0000

    Align Core templare with dynamic Close block

--- lib/experimental/navigation-overlay.php
+++ lib/experimental/navigation-overlay.php
@@ -1,8 +1,6 @@
 <!-- wp:group {"lock":{"move":true,"remove":true},"metadata":{"name":"<?php echo __('Overlay');?>"},"style":{"dimensions":{"minHeight":"100vh"},"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30","left":"var:preset|spacing|30","right":"var:preset|spacing|30"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
 <div class="wp-block-group" style="min-height:100vh;padding-top:var(--wp--preset--spacing--30);padding-right:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30);padding-left:var(--wp--preset--spacing--30)"><!-- wp:group {"metadata":{"name":"Close"},"align":"wide","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
-<div class="wp-block-group alignwide"><!-- wp:navigation-overlay-close {"lock":{"move":false,"remove":true},"metadata":{}} -->
-<button class="wp-block-navigation-overlay-close" aria-label="<?php echo esc_attr_x('Close menu','Alternative label for closing the overlay'); ?>"><?php echo esc_html_e('Close menu'); ?></button>
-<!-- /wp:navigation-overlay-close --></div>
+<div class="wp-block-group alignwide"><!-- wp:navigation-overlay-close {"lock":{"move":false,"remove":true},"metadata":{},"className":"wp-block-navigation-overlay-close"} /--></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"metadata":{"name":"<?php echo __('Contents'); ?>"},"align":"wide","style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","orientation":"vertical","justifyContent":"right"}} -->
