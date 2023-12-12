1ad7ecf734d1e875ffe15986d9b74f466267bf0a
scruffian <ben@scruffian.com>
2023-12-12 12:38:23 +0000

    allow the close button to close the overlay editor

--- packages/block-library/src/navigation-overlay-close/edit.js
+++ packages/block-library/src/navigation-overlay-close/edit.js
@@ -5,12 +5,30 @@ import { Button, Icon, ToggleControl, PanelBody } from '@wordpress/components';
 import { close } from '@wordpress/icons';
 import { __ } from '@wordpress/i18n';
 import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { privateApis as routerPrivateApis } from '@wordpress/router';
 
-export default function Edit( { attributes, setAttributes } ) {
+/**
+ * Internal dependencies
+ */
+import { unlock } from '../lock-unlock';
+
+const { useHistory } = unlock( routerPrivateApis );
+
+export default function Edit( { attributes, isSelected, setAttributes } ) {
 	const blockProps = useBlockProps();
+	const history = useHistory();
 	const { hasIcon } = attributes;
 
 	const closeText = __( 'Close' );
+
+	const onClick = () => {
+		if ( isSelected ) {
+			// Exit navigation overlay edit mode.
+			history.back();
+		}
+	};
+
+	blockProps.onClick = onClick;
 
 	return (
 		<>
