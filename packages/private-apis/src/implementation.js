e07eb476655bf9b4ccc49af8f28e0cc56a1fff48
chad1008 <13856531+chad1008@users.noreply.github.com>
2023-12-20 20:52:24 -0500

    add support for private apis

--- packages/private-apis/src/implementation.js
+++ packages/private-apis/src/implementation.js
@@ -24,6 +24,7 @@ const CORE_MODULES_USING_PRIVATE_APIS = [
 	'@wordpress/edit-site',
 	'@wordpress/edit-widgets',
 	'@wordpress/editor',
+	'@wordpress/interface',
 	'@wordpress/patterns',
 	'@wordpress/reusable-blocks',
 	'@wordpress/router',
